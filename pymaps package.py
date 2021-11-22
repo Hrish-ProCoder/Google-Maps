ae4fae40c1d59bf2c3868af3d929915a693587e7
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-22 06:57:07 +0530

    Update pymaps package.py

--- pymaps package.py
+++ pymaps package.py
@@ -1,4 +1,7 @@
-# First install the packages by following below steep
+# First install the packages by following below step
 # pip install pygmaps
 
 # maps method return map object
+# 1st argument is center latitude
+# 2nd argument is center longitude
+# 3rd argument is zoom level
