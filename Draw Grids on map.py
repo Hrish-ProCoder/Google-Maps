c23eb4a6fb2d2e6a2af1fe203e74e0740dfa4579
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-24 07:39:13 +0530

    Update Draw Grids on map.py

--- Draw Grids on map.py
+++ Draw Grids on map.py
@@ -11,3 +11,6 @@ map2 = pygmaps.maps(17.2156831, 56.03222175623432, 15)
 # 4th argument is the starting point of longitude
 # 5th argument is the ending point of longitude
 # 6th argument is grid size in longitude
+
+maps2.setgrids(17.23, 17.24, 0.001, 
+               56.03, 56.04, 0.001)
