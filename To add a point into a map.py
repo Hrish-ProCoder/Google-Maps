f44cd211e0e18b4d4eb617b167019a724976898c
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-26 00:19:07 +0530

    Update To add a point into a map.py

--- To add a point into a map.py
+++ To add a point into a map.py
@@ -7,11 +7,11 @@ latitude_list = [17.3358376, 17.307977, 17.3216419, 17.3427904, 17.378598, 17.35
 longitude_list = [57.8701919, 58.048457, 58.0413095, 57.886958, 57.825396, 57.8460573, 58.0537813, 58.090614, 58.0355272,
                   57.9311923, 57.9663, 57.952092, 58.0747887, 57.9555512, 57.9997158]
 for i in range(len(latitude_list)):
- # add a point into a map
-# 1st argument is latitude
-# 2nd argument is longitude
-# 3rd argument is colour of the point showed in thed map
-# using HTML colour code
-# red "# FF0000", Blue "# 0000FF", Green "# 00FF00"
-
-mymap3.addpoint(latitude_list[i], longitude_list[i], "# FF0000")
+  # add a point into a map
+  # 1st argument is latitude
+  # 2nd argument is longitude
+  # 3rd argument is colour of the point showed in thed map
+  # using HTML colour code
+  # red "# FF0000", Blue "# 0000FF", Green "# 00FF00"
+  mymap3.addpoint(latitude_list[i], longitude_list[i], "# FF0000")
+mymap3.draw('pygmap3.html')
