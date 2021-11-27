bea70d77ec0f95a11ac7faaf35bcab12ccdb09e3
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-27 14:55:13 +0530

    Update To Draw a circle of given radius.py

--- To Draw a circle of given radius.py
+++ To Draw a circle of given radius.py
@@ -6,3 +6,4 @@ mymap4 = pygmaps.maps(17.3164945, 57.03219179999999, 15)
 # 3rd argument is radius (in meter)
 # 4th argument is colour of the circle
 mymap4.addradpoint(17.307977, 57.048457, 95, "# FF0000")
+mymap4.draw('pygmap4.html')
