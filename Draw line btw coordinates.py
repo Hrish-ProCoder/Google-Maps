9a573f256f02c72dd6fd6a83cbc71b7ed99e1b99
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-29 18:30:41 +0530

    Update Draw line btw coordinates.py

--- Draw line btw coordinates.py
+++ Draw line btw coordinates.py
@@ -2,3 +2,5 @@ import pygmaps
 mymap5 = pygmaps.maps(17.3164945, 57.03219179999999, 15)
 latitude_list =[17.343769, 17.307977]
 longitude_list =[56.999559, 57.048457]
+for i in range(len(latitude_list)) :
+  mymap5.addpoint(latitude_list[i], longitude_list[i], "# FF0000")
