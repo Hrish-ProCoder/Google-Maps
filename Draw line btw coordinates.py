d4ba79c25de092b7e3e633376a8f3d3f89ca014d
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-11-29 18:31:35 +0530

    Update Draw line btw coordinates.py

--- Draw line btw coordinates.py
+++ Draw line btw coordinates.py
@@ -4,3 +4,5 @@ latitude_list =[17.343769, 17.307977]
 longitude_list =[56.999559, 57.048457]
 for i in range(len(latitude_list)) :
   mymap5.addpoint(latitude_list[i], longitude_list[i], "# FF0000")
+# list of coordinates         
+path = [(17.343769, 57.999559), (17.307977, 57.048457)]
