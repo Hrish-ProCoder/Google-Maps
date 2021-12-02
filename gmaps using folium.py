1f00cc490aff8b39a5e9b8901284df5fbf6753b1
Hrish-ProCoder <65326273+Hrish-ProCoder@users.noreply.github.com>
2021-12-02 07:46:45 +0530

    Update gmaps using folium.py

--- gmaps using folium.py
+++ gmaps using folium.py
@@ -3,3 +3,5 @@ import folium
 # Map method of folium return Map object
 
 my_map1 = folium.Map(location = [18.6111226, 57.2199794], zoom_start = 14 )
+
+# save method of Map object will create a map
